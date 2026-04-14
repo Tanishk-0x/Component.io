b0273cad26f0b6cdc4b880437c1b80d97f0f8508
Tanishk-0x <tanishknamdev981@gmail.com>
2026-04-14 23:08:51 +0530

    User Model

---
+++ Backend/src/Models/userModel.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+
+    name: {
+        type: String , 
+        required: true ,
+        trim: true
+    },
+    email: {
+        type: String , 
+        required: true ,
+        trim: true
+    },
+    password: {
+        type: String , 
+        required: true ,
+        trim: true
+    },
+
+    credits: {
+        type: Number,
+        default: 100
+    },
+    savedComponents: [
+        {
+            type: mongoose.Schema.Types.ObjectId ,
+            ref: "Component"
+        }
+    ],
+    isVerified: {
+        type: Boolean,
+        default: false
+    }
+
+
+}, {timestamps: true}); 
+
+const User = mongoose.model("User" , UserSchema); 
+export default User ;
