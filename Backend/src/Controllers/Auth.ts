885971a7eedd3ffb066ea5886ab5c4631a80bfcc
Tanishk-0x <tanishknamdev981@gmail.com>
2026-04-17 03:24:35 +0530

    Refresh Token

--- Backend/src/Controllers/Auth.ts
+++ Backend/src/Controllers/Auth.ts
@@ -2,12 +2,15 @@ import User from "../Models/userModel";
 import bcrypt from 'bcrypt'; 
 import { Request , Response } from "express";
 import {SetAccessToken , SetRefreshToken , SetCsrfToken , ClearAllCookies} from '../Utils/tokens' ; 
+import jwt, { JwtPayload } from 'jsonwebtoken'; 
+
+const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || '' ;
 
 // Token Configs (MaxAge)
 const Config = {
-    AccessMaxAge: 15 * 60 * 1000,
-    RefreshMaxAge: 60 * 60 * 1000, 
-    CsrfMaxAge: 60 * 60 * 1000, 
+    AccessMaxAge:  15 * 1000,
+    RefreshMaxAge:  60 * 1000, 
+    CsrfMaxAge:  60 * 1000, 
 }
 
 // --------- SIGNUP HANDLER ----------
@@ -123,6 +126,59 @@ export const Logout = async (req: Request , res: Response) => {
         return res.status(500).json({
             success: false , 
             message: 'Error While Logout' , 
+            error: error
+        }); 
+    }
+}
+
+
+// --------- REFRESH TOKEN ----------
+export const RefreshToken = async (req: Request, res: Response) => {
+    try {
+        const refreshToken = req.cookies.refresh_token ; 
+
+        if( !refreshToken ){
+            return res.status(401).json({
+                success: false,
+                message: 'UnAuthorized! (Refresh Token Missing)'
+            });
+        }
+
+        const decoded = jwt.verify( refreshToken , REFRESH_TOKEN_SECRET ) as JwtPayload ; 
+
+        if( !decoded.userId || !decoded.exp ){
+            return res.status(401).json({
+                success: false,
+                message: 'UnAuthorized! (Invalid Refresh Token)'
+            }); 
+        }
+
+        const CurrentTime = Math.floor(Date.now() / 1000); 
+        const RemainingTime = decoded.exp - CurrentTime ; 
+
+        if( RemainingTime <= 0 ){
+            return res.status(401).json({
+                success: false,
+                message: 'UnAuthorized! (Refresh Token Expired)'
+            });
+        }
+
+        // Refreshing Token 
+        SetAccessToken(res , String(decoded.userId) , 'user' ,  Config.AccessMaxAge); 
+        SetRefreshToken(res , String(decoded.userId) , 'user' ,  RemainingTime * 1000); 
+        SetCsrfToken(res , RemainingTime * 1000); 
+
+        return res.status(200).json({
+            success: true ,
+            message: 'Token Refreshed!'
+        }); 
+    }
+    
+    catch (error) {
+        console.error('Refreshing Token Error: ' , error);
+        return res.status(500).json({
+            success: false , 
+            message: 'Error While Refreshing Token' , 
             error: error
         }); 
     }
