85355ca2492445832bb208ae1d806de9f575f8bf
Tanishk-0x <tanishknamdev981@gmail.com>
2026-04-17 00:14:28 +0530

    MaiUrlContext

---
+++ Frontend/src/Context/MainContext.tsx
@@ -0,0 +1,34 @@
+import { createContext } from "react"
+import type{ ReactNode } from "react";
+
+interface MainContextType {
+    serverUrl: string;
+}
+
+interface MainContextProps {
+    children: ReactNode; 
+}
+
+// Creating Context 
+export const mainUrlContext = createContext <MainContextType | null>(null); 
+
+const MainContext = ({ children }: MainContextProps ) => {
+
+    const serverUrl = 'http://localhost:8000' ; 
+
+    // Defining Values 
+    const value = {
+        serverUrl
+    }; 
+
+    return (
+        <div>
+            {/* Providing Context  */}
+            <mainUrlContext.Provider value={value}>
+                {children}
+            </mainUrlContext.Provider>
+        </div>
+    )
+}
+
+export default MainContext
