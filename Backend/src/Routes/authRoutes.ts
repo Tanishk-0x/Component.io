3da8a1ef0f4bc8fc8a81d7f93d00a7a6974bdbba
Tanishk-0x <tanishknamdev981@gmail.com>
2026-04-17 20:32:52 +0530

    Minor Route Change

--- Backend/src/Routes/authRoutes.ts
+++ Backend/src/Routes/authRoutes.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post('/signup' , Signup); 
 router.post('/login' , Login); 
-router.post('/logout' , Logout); 
+router.post('/logout' , requireCSRF , Logout); 
 
 router.post('/refresh' , requireCSRF , RefreshToken); 
 
