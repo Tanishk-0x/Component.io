bf57195997939f55c3d0f9ad8bf40f5d85cf074a
Tanishk-0x <tanishknamdev981@gmail.com>
2026-05-06 15:27:32 +0530

    CLI Implementation

---
+++ Cli/bin/index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import fs from 'fs' ; 
+
+const [, , command, id] = process.argv ; 
+const BASE_URL = "http://localhost:8000"
+
+
+const RUN = async () => {
+    if( command === 'get' && id ){
+
+        try {
+            console.log("⏳ Fetching from Component.io...");
+            // API Call 
+            const res = await fetch(`${BASE_URL}/cli/get-component/${id}`); 
+            const data = await res.json(); 
+
+            if( data.success && data.fileName ){
+                // Creating File ... 
+                fs.writeFileSync( data.fileName , data.code); 
+
+                console.log(`✅ Done ${data.fileName} Created!`); 
+            }
+            else{
+                console.log("❌ Error:", data.message);
+            }
+        }
+
+        catch (error) {
+            console.error("❌ Connection failed!");
+        }
+    }
+
+    else{
+        console.log("Usage: npx component-io get <id>");
+    }
+};
+
+RUN(); 
